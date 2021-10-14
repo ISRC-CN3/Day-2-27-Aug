a92603b6389e48c6faf1aaa4e258ef6e205166b0
mac-n <39732428+mac-n@users.noreply.github.com>
2021-10-14 17:47:53 +0100

    Lab codes and notes

---
+++ Lab/Codes/Khamassi_RLlab/MDPStep.m
@@ -0,0 +1,6 @@
+function [y, r] = MDPStep(M, x, u)
+% This function executes a step on the MDP M given current state x and action u.
+% It returns a next state y and a reward r
+
+y = ???;
+r = ???;
---
+++ Lab/Codes/Khamassi_RLlab/RTDP.m
@@ -0,0 +1,37 @@
+function [Q,pol] = RTDP(M)
+% This function computes the optimal Q-function and the corresponding policy
+% for the MDP M using Real-Time DP
+
+init;
+
+% Initialize the Q-function
+Q = ???;
+
+% Initialize the transition model and the number of visits for each state-action pair
+hatP = ???;
+N    = ???;
+
+% Draw an initial state
+x = ???
+
+% Run learning cycle
+nbIter = ???;
+for iter = 1:nbIter
+    
+    % Draw a random action and get the next state and reward
+    u = ???;
+    [y, r] = ???;
+    
+    % Update transition matrix (stochastic)
+    hatP(x,u,:) = ???;
+    
+    % Update Q
+    Q(x, u) = ???;
+    
+    % Update the number of visits for the current state-action pair
+    N(x, u) = ???;
+    
+    % The next state becomes the current state
+    x = y;
+end
+% Compute a policy based on the estimate of the optimal Q-functionpol = ???;
